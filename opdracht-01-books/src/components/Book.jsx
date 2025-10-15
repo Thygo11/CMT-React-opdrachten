39080636b742ba417c8ca8377bc18709d87fc352
Thygo11 <9025896@student.zadkine.nl>
2025-10-15 11:04:49 +0200

    Update Book.jsx

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function Book({ title, author, image }) {
+  const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+
+  function verhoogTeller() {
+    setAantalKeerGelezen(aantalKeerGelezen + 1);
+  }
+
   return (
     <div className="book">
       <img src={image} alt={title} className="book-image" />
       <h3>{title}</h3>
       <p>{author}</p>
+
+      
+      <button onClick={verhoogTeller}>Ik heb dit boek gelezen</button>
+      <p>Aantal keer gelezen: {aantalKeerGelezen}</p>
     </div>
   );
 }
